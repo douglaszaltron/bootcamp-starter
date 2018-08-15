e1439cb97b52a1cdbc2f8584c9b831a8352c4c11
Douglas <douglaszaltron@gmail.com>
2018-08-15 17:39:36 -0300

    Promises

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var pessoa = { nome: 'Douglas' }
-var idade = { idade: 31 }
+let promise = new Promise((resolve, reject) => {
 
-Object.assign( pessoa, idade )
+  /*
+  const FLAG = 5
 
-console.log( pessoa )
+  if (FLAG == 5)
+    resolve('Tudo pronto!')
+  else
+    reject(Error('Algo deu errado!'))
+  */
+
+  // setTimeout(() => resolve('Tudo pronto!'), 3000)
+
+  setTimeout(resolve, 4000, 'Tudo pronto')
+
+})
+
+promise.then(response => console.log(response)).catch(reason => console.log(reason))
