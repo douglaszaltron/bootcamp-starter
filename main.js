f48af146ef250985f0117567af3daff4e239f313
Douglas <douglaszaltron@gmail.com>
2018-08-15 16:33:12 -0300

    Arrow functions

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const newArr = arr.map(function(item, index){
-    return item + index
-   
-})
+const newArr = arr.map((item, index) => item + index)
 
 console.log(newArr)
 
-const sum = arr.reduce(function(total, next){
-    return total + next;
-})
+const sum = arr.reduce((total, next) => total + next)
 
 console.log(sum)
 
-const filter = arr.filter(function( item ){
-    return item % 2 === 0
-}) 
+const filter = arr.filter(item => item % 2 === 0)
 
 console.log(filter)
 
-const find = arr.find(function(item){
-    return item === 1
-})
+const find = arr.find(item => item === 1)
 
-console.log (find)
+console.log(find)
