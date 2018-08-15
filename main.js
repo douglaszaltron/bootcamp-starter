301ce52b8642920371fb7a54cecb70c934a21071
Douglas <douglaszaltron@gmail.com>
2018-08-15 16:38:06 -0300

    Arrow functions em constantes

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const teste = () => ({ nome: 'douglas' })
 
-const newArr = arr.map((item, index) => item + index)
-
-console.log(newArr)
-
-const sum = arr.reduce((total, next) => total + next)
-
-console.log(sum)
-
-const filter = arr.filter(item => item % 2 === 0)
-
-console.log(filter)
-
-const find = arr.find(item => item === 1)
-
-console.log(find)
+console.log(teste())
