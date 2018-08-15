702bb356fa11fedf4c6084cc13a9ef942db105e5
Douglas <douglaszaltron@gmail.com>
2018-08-15 16:39:53 -0300

    Desestruturação

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-const soma = (a = 0, b = 2) => a + b
-
-console.log(soma(1))
-console.log(soma())
+const usuario = {
+    nome: 'Douglas',
+    idade: '31',
+    endereco: {
+      cidade: 'Rio verde',
+      estado: 'GO',
+    },
+  }
+  
+  const { nome, idade, endereco: { cidade } } = usuario
+  
+  console.log(nome)
+  console.log(idade)
+  console.log(cidade)
+  
+  function mostraNome ({ nome, idade }) {
+    console.log(nome, idade)
+  }
+  
+  mostraNome(usuario)
+  
