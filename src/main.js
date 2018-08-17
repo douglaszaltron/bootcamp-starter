08c80f8d801a6b43998017eb944a2e3cc6d9f342
Douglas <douglaszaltron@gmail.com>
2018-08-17 16:28:14 -0300

    Render em tela

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ class App {
 
     /* global document */
     this.formEl = document.getElementById('repo-form')
+    this.listEl = document.getElementById('repo-list')
     this.registerHandlers()
   }
 
@@ -21,7 +22,34 @@ class App {
       html_url: 'https://github.com/adonisjs/adonis-framework',
     })
 
-    console.log(this.repositories)
+    this.render()
+  }
+
+  render () {
+    this.listEl.innerHTML = ''
+
+    this.repositories.forEach((repo) => {
+      const imgEl = document.createElement('img')
+      imgEl.setAttribute('src', repo.avatar_url)
+
+      const titleEl = document.createElement('strong')
+      titleEl.appendChild(document.createTextNode(repo.name))
+
+      const descriptionEl = document.createElement('p')
+      descriptionEl.appendChild(document.createTextNode(repo.description))
+
+      const linkEl = document.createElement('a')
+      linkEl.setAttribute('target', '_blank')
+      linkEl.appendChild(document.createTextNode('Acessar'))
+
+      const listItemEl = document.createElement('li')
+      listItemEl.appendChild(imgEl)
+      listItemEl.appendChild(titleEl)
+      listItemEl.appendChild(descriptionEl)
+      listItemEl.appendChild(linkEl)
+
+      this.listEl.appendChild(listItemEl)
+    })
   }
 }
 
