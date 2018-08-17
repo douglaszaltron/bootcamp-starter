1392574888658be27935f9973e08c94ea4697acb
Douglas <douglaszaltron@gmail.com>
2018-08-17 16:19:47 -0300

    Adicionar repositórios

--- src/main.js
+++ src/main.js
@@ -1,14 +1,29 @@
-import axios from 'axios'
+class App {
+  constructor () {
+    this.repositories = []
 
-class Api {
-  static async getUserInfo (username) {
-    try {
-      const response = await axios.get(`https://api.github.com/users/${username}`)
-      console.log(response.data)
-    } catch (error) {
-      console.log(`Reason: ${error}`)
-    }
+    /* global document */
+    this.formEl = document.getElementById('repo-form')
+    this.registerHandlers()
+  }
+
+  registerHandlers () {
+    this.formEl.onsubmit = event => this.addRepository(event)
+  }
+
+  addRepository (event) {
+    event.preventDefault()
+
+    this.repositories.push({
+      name: 'Adonis Framework',
+      description: 'NodeJs Web Application Framework. Makes it easy for you to write webapps with less code.',
+      avatar_url: 'https://avatars2.githubusercontent.com/u/13810373?s=200&v=4',
+      html_url: 'https://github.com/adonisjs/adonis-framework',
+    })
+
+    console.log(this.repositories)
   }
 }
 
-Api.getUserInfo('douglaszaltron')
+/* eslint no-unused-vars: 0 */
+const app = new App()
