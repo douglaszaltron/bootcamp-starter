92611d323bef4b485da8998a407dd3af1b16da7e
Douglas <douglaszaltron@gmail.com>
2018-08-17 15:31:17 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('Hello world')
+const aguarda = () => new Promise((resolve) => {
+  setTimeout(() => { resolve('OK') }, 2000)
+})
+
+const executa = async () => {
+  console.log(await aguarda())
+}
+
+executa()
